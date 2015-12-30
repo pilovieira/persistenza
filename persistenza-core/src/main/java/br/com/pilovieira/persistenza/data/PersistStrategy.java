eaedd3c4a6e25d5d58d29c448e03e1cd137e0085
Luiz Paulo Vieira <pilovieira@gmail.com>
2015-12-30 20:25:24 +0000

    yolo buff mode

---
+++ persistenza-core/src/main/java/br/com/pilovieira/persistenza/data/PersistStrategy.java
@@ -0,0 +1,16 @@
+package br.com.pilovieira.persistenza.data;
+
+import br.com.pilovieira.persistenza.PersistenzaManager;
+
+
+public abstract class PersistStrategy {
+	
+	protected SessionManager sessionManager = new SessionManager(PersistenzaManager.getFactory());
+	
+	public abstract void persist(final Object... entities);
+
+	public abstract void delete(final Object... entities);
+
+	public abstract void apply();
+	
+}
