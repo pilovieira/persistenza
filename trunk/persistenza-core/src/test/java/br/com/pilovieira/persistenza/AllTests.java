82e6929f53e38eecf4647c1ccc0acae5ff6a98d9
tiopilo <tiopilo@b74ab0be-8ea3-4054-8d33-2d96e17e8a69>
2016-01-13 20:39:47 +0000

    buffer testes funcionais

--- trunk/persistenza-core/src/test/java/br/com/pilovieira/persistenza/AllTests.java
+++ trunk/persistenza-core/src/test/java/br/com/pilovieira/persistenza/AllTests.java
@@ -13,6 +13,7 @@ import br.com.pilovieira.persistenza.data.SessionManagerTest;
 import br.com.pilovieira.persistenza.data.YoloTest;
 import br.com.pilovieira.persistenza.db.HyperSqlTest;
 import br.com.pilovieira.persistenza.db.PostgreSqlTest;
+import br.com.pilovieira.persistenza.functional.PersistBufferFunctionalTest;
 import br.com.pilovieira.persistenza.functional.PersistYoloFunctionalTest;
 import br.com.pilovieira.persistenza.functional.PersistenzaGetFunctionalTest;
 
@@ -35,6 +36,7 @@ import br.com.pilovieira.persistenza.functional.PersistenzaGetFunctionalTest;
 			
 			PersistenzaGetFunctionalTest.class,
 			PersistYoloFunctionalTest.class,
+			PersistBufferFunctionalTest.class
 		})
 public class AllTests {
 
