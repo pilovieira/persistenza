c062e783658d508bc9144f0add3eec5d718585ef
tiopilo <tiopilo@b74ab0be-8ea3-4054-8d33-2d96e17e8a69>
2016-01-13 17:09:54 +0000

    cobertura de testes

--- trunk/persistenza-core/src/main/java/br/com/pilovieira/persistenza/data/PersistStrategy.java
+++ trunk/persistenza-core/src/main/java/br/com/pilovieira/persistenza/data/PersistStrategy.java
@@ -1,11 +1,10 @@
 package br.com.pilovieira.persistenza.data;
 
-import br.com.pilovieira.persistenza.PersistenzaManager;
 
 
 abstract class PersistStrategy {
 	
-	protected SessionManager sessionManager = new SessionManager(PersistenzaManager.getFactory());
+	protected SessionManager sessionManager = SessionManager.getInstance();
 	
 	public abstract void persist(final Object... entities);
 
--- trunk/persistenza-core/src/test/java/br/com/pilovieira/persistenza/db/PostgreSqlTest.java
+++ trunk/persistenza-core/src/test/java/br/com/pilovieira/persistenza/db/PostgreSqlTest.java
@@ -1,14 +1,33 @@
 package br.com.pilovieira.persistenza.db;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PostgreSqlTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private PostgreSql subject;
+
+	@Before
+	public void setup() {
+		subject = new PostgreSql("url", "user", "pass");
 	}
+	
+	@Test
+	public void getDialect() {
+		assertEquals("Dialect", "org.hibernate.dialect.PostgreSQLDialect", subject.getDialect());
+	}
+
+	@Test
+	public void getConnectionDriverClass() {
+		assertEquals("Driver", "org.postgresql.Driver", subject.getConnectionDriverClass());
+	}
+
+	@Test
+	public void getSslFactory() {
+		assertEquals("SSL Factory", "org.postgresql.ssl.NonValidatingFactory", subject.getSslFactory());
+	}
+
 
 }
