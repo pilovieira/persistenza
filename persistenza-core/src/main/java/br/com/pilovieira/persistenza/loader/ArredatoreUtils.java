6a0d05ecb3dcb3c93eedfdd4d1f3cebcca7a2ca0
pilovieira <pilovieira@gmail.com>
2020-01-29 01:26:12 -0300

    Revert "class"
    This reverts commit cc478245

--- persistenza-core/src/main/java/br/com/pilovieira/persistenza/loader/ArredatoreUtils.java
+++ persistenza-core/src/main/java/br/com/pilovieira/persistenza/loader/ArredatoreUtils.java
@@ -1,17 +1,10 @@
 package br.com.pilovieira.persistenza.loader;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import javassist.CtClass;
 import javassist.CtField;
-import javassist.NotFoundException;
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.ClassFile;
 import javassist.bytecode.ConstPool;
@@ -50,41 +43,6 @@ class ArredatoreUtils {
 			attr.addAnnotation(new Annotation(annotation.getName(), cpool));
 		
 		ctField.getFieldInfo().addAttribute(attr);
-	}
-	
-	public static boolean isAutoMapped(CtField field) throws NotFoundException {
-		CtClass type = field.getType();
-		return type.isPrimitive() || ArredatoreUtils.getAutoMappedTypes().contains(field.getType().getName()); 
-	}
-	
-	private static List<String> getAutoMappedTypes() {
-		List<String> types = new ArrayList<>();
-		
-		types.add(Boolean.class.getName());
-		types.add(Character.class.getName());
-		types.add(Date.class.getName());
-		types.add(Double.class.getName());
-		types.add(Float.class.getName());
-		types.add(Integer.class.getName());
-		types.add(Long.class.getName());
-		types.add(Short.class.getName());
-		types.add(String.class.getName());
-		
-		return types;
-	}
-	
-	public static boolean isTransient(CtField ctField) {
-		return ctField.hasAnnotation(javax.persistence.Transient.class);
-	}
-	
-	public static boolean hasAnyRelashionshipAnnotations(CtField field) {
-		Class<?>[] annotations = {OneToOne.class, OneToMany.class, ManyToOne.class, ManyToMany.class};
-		
-		for (Class<?> annotation : annotations)
-			if (field.hasAnnotation(annotation))
-				return true;
-		
-		return false;
 	}
 
 }
